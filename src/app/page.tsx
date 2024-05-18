6be1fb3308ac18a3204b93a64dd37114da4bedba
Francisco <javiermartincuenca@gmail.com>
2024-05-18 10:08:22 +0200

    Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Canva</h1>
-    </main>
+    <div className='bg-[#18191b] min-h-screen w-full'>
+      <div className='bg-[#212223] shadow-md'>
+        <div className='w-[93%] m-auto py-3'>
+          <div className='flex justify-between items-center'>
+            <div className='w-[80px] h-[48px]'>
+              <img
+                src='https://static.canva.com/web/images/12487a1e0770d29351bd4ce4f87ec8fe.svg'
+                alt='Logo'
+                role='presentation'
+                width={80}
+                height={30}
+                className='size-full'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
