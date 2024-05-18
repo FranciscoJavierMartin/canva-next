50cdaf1dfb3c56e153cda2dc37dc83b030372f38
Francisco <javiermartincuenca@gmail.com>
2024-05-18 13:26:39 +0200

    Home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,29 @@ export default function Home() {
                 className='size-full'
               />
             </div>
+            <div className='flex gap-4'>
+              <button className='py-2 w-[80px] text-center bg-teal-700 text-white  rounded-md font-medium transition-all hover:bg-teal-500'>
+                Sign In
+              </button>
+              <button className='py-2 w-[80px] text-center bg-purple-700 text-white  rounded-md font-medium transition-all hover:bg-purple-500'>
+                Sign Up
+              </button>
+            </div>
           </div>
+        </div>
+      </div>
+      {/* Flex? */}
+      <div className='size-full justify-center items-center p-4'>
+        <div className='py-[170px] flex justify-center items-center flex-col gap-6'>
+          <h2 className='text-5xl text-[#c7c5c5] font-bold'>
+            What you will design today?
+          </h2>
+          <span className='text-[#aca9a9] text-2xl font-medium'>
+            Canva makes it easy to create and share professional designs.
+          </span>
+          <button className='py-2 w-[200px] text-center bg-purple-700 text-white rounded-md font-medium transition-all hover:bg-purple-500'>
+            Sign Up for free
+          </button>
         </div>
       </div>
     </div>
