2a42a3b2f2c5fe0e83f6a5344d4a930db2b909c4
Francisco <javiermartincuenca@gmail.com>
2024-05-20 21:05:43 +0200

    Add password field

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,13 +4,14 @@ import InputForm from './InputForm';
 
 export default function LoginForm() {
   const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   return (
     <>
       <h2 className='pb-4 text-center text-xl text-white'>
         Login and Sign up in seconds
       </h2>
-      <form>
+      <form className='flex flex-col gap-2'>
         <InputForm
           inputProps={{
             id: 'email',
@@ -21,6 +22,17 @@ export default function LoginForm() {
           labelText='Email'
           setValue={setEmail}
           value={email}
+        />
+        <InputForm
+          inputProps={{
+            id: 'password',
+            name: 'password',
+            placeholder: 'Password',
+            type: 'password',
+          }}
+          labelText='Password'
+          setValue={setPassword}
+          value={password}
         />
       </form>
     </>
