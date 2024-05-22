25688363195a36e7f5cdca6f42e8e9e76282fb39
Francisco <javiermartincuenca@gmail.com>
2024-05-22 22:09:11 +0200

    Setup create design page

---
+++ src/app/design/create/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useRef } from 'react';
+import CreateComponent from '@/components/CreateComponent';
+
+export default function CreateDesignPage() {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const obj = {
+    name: 'main_frame',
+    type: 'rect',
+    id: Math.floor(Math.random() * 100 + 1),
+    height: '200',
+    width: '400',
+    z_index: 1,
+    color: 'green',
+    image:
+      'https://www.startpage.com/av/proxy-image?piurl=http%3A%2F%2Fwww.wallpapers13.com%2Fwp-content%2Fuploads%2F2015%2F12%2FNature-Lake-Bled.-Desktop-background-image.jpg&sp=1716398712T6c0cfe91bc16bd680cc376ca747add95d22695488724690775950447b886e1c7',
+  };
+
+  return (
+    <div className='relative flex h-screen w-screen items-center justify-center'>
+      <div ref={ref} className='relative size-auto overflow-auto'>
+        <CreateComponent info={obj} currentComponent={{}} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/CreateComponent.tsx
@@ -0,0 +1,33 @@
+import React, { Attributes, HTMLAttributes, ReactNode } from 'react';
+
+type CreateComponentProps = {
+  info: any;
+  currentComponent: object;
+};
+
+export default function CreateComponent({
+  info,
+  currentComponent,
+}: CreateComponentProps) {
+  let html: ReactNode = '';
+
+  if (info.name === 'main_frame') {
+    html = (
+      <div
+        className='shadow-md hover:border-2 hover:border-indigo-500'
+        style={{
+          width: info.width + 'px',
+          height: info.height + 'px',
+          background: info.color,
+          zIndex: info.z_index,
+        }}
+      >
+        {info.image && (
+          <img className='size-full' src={info.image} alt='image' />
+        )}
+      </div>
+    );
+  }
+
+  return html;
+}
