09b506c2d9a7c66aeb37c186f73f586633106c7d
Francisco <javiermartincuenca@gmail.com>
2024-05-23 19:06:06 +0200

    Image gallery

--- src/app/design/[id]/edit/page.tsx
+++ src/app/design/[id]/edit/page.tsx
@@ -13,6 +13,7 @@ import { RxTransparencyGrid } from 'react-icons/rx';
 import Header from '@/components/Header';
 import TemplateDesign from '@/components/sidebar/TemplateDesign';
 import MyImages from '@/components/sidebar/MyImages';
+import ImageGallery from '@/components/sidebar/ImageGallery';
 
 type ElementType =
   | 'design'
@@ -106,7 +107,7 @@ export default function EditDesignPage() {
         </div>
         <div className='h-full w-[calc(100%-80px)]'>
           {/* TODO: Add styles to close */}
-          <div className='fixed left-[80px] z-30 h-full w-[350px]  bg-[#252627] px-8 py-5 transition-all duration-700'>
+          <div className='fixed left-[80px] z-30 h-full w-[350px] bg-[#252627] px-8 py-5 transition-all duration-700'>
             <button
               onClick={() => setShow({ name: '', status: true })}
               className='absolute -right-2 top-[40%] flex h-[100px] w-[20px] cursor-pointer items-center justify-center rounded-full bg-[#252627] text-slate-300'
@@ -128,15 +129,21 @@ export default function EditDesignPage() {
                 ></div>
               </div>
             )}
-            {state === 'image' && (
-              <MyImages/>
+            {state === 'image' && <MyImages />}
+            {state === 'text' && (
+              <div className='grid grid-cols-1 gap-2'>
+                <div className='cursor-pointer rounded-sm bg-[#3c3c3d] p-3 text-xl font-bold text-white'>
+                  <h2>Add a text</h2>
+                </div>
+              </div>
             )}
-            {state === 'text' && <div className='grid grid-cols-2 gap-2'></div>}
             {state === 'project' && (
               <div className='grid grid-cols-2 gap-2'></div>
             )}
             {state === 'initImage' && (
-              <div className='grid grid-cols-2 gap-2'></div>
+              <div className='no-scrollbar flex h-[88vh] items-start justify-start overflow-x-auto'>
+                <ImageGallery />
+              </div>
             )}
             {state === 'background' && (
               <div className='grid grid-cols-2 gap-2'></div>
