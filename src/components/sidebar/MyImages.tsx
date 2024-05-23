c2fd7e7f9362212878380fcaba42ba307032f3c0
Francisco <javiermartincuenca@gmail.com>
2024-05-23 18:50:20 +0200

    Image upload

--- src/components/sidebar/MyImages.tsx
+++ src/components/sidebar/MyImages.tsx
@@ -1,9 +1,16 @@
+import ImageGallery from '@/components/sidebar/ImageGallery';
+
 export default function MyImages() {
   return (
     <div>
-      <div>
-        <label htmlFor='image'>Upload image</label>
+      <div className='mb-3 flex h-[40px] w-full items-center justify-center rounded-md bg-purple-500 text-white'>
+        <label htmlFor='image' className='cursor-pointer text-center'>
+          Upload image
+        </label>
         <input type='file' id='image' className='hidden' />
+      </div>
+      <div className='no-scrollbar flex h-[80vh] items-start justify-start overflow-x-auto'>
+        <ImageGallery />
       </div>
     </div>
   );
--- src/components/sidebar/TemplateDesign.tsx
+++ src/components/sidebar/TemplateDesign.tsx
@@ -8,7 +8,7 @@ export default function TemplateDesign() {
         >
           <img
             className='size-full'
-            src='https://www.startpage.com/av/proxy-image?piurl=http%3A%2F%2Fwww.wallpapers13.com%2Fwp-content%2Fuploads%2F2015%2F12%2FNature-Lake-Bled.-Desktop-background-image.jpg&sp=1716398712T6c0cfe91bc16bd680cc376ca747add95d22695488724690775950447b886e1c7'
+            src='http://localhost:4200/proxy-image.jpg'
             alt=''
           />
         </div>
