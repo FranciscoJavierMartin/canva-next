4965f3d574417a9ed74febcde1020ae1d24273e5
Francisco <javiermartincuenca@gmail.com>
2024-05-24 19:18:14 +0200

    Move elements icons

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -6,6 +6,7 @@ type ElementProps = {
 
 export default function Element({ id, info, exId }: ElementProps) {
   // TODO: Check exId
+  // TODO: Change rotate cursor icon
   return (
     <>
       {exId ? (
@@ -25,7 +26,7 @@ export default function Element({ id, info, exId }: ElementProps) {
         </>
       ) : (
         <>
-         <div
+          <div
             onMouseDown={info.resizeElement(id, info)}
             className='absolute -bottom-[3px] -right-[3px] z-50 hidden size-[10px] cursor-nesw-resize bg-green-600 group-hover:block'
           ></div>
@@ -42,6 +43,22 @@ export default function Element({ id, info, exId }: ElementProps) {
       <div
         onMouseDown={info.rotateElement(id, info)}
         className='absolute -left-[3px] -top-[3px] z-50 hidden size-[10px] cursor-nesw-resize bg-red-600 group-hover:block'
+      ></div>
+      <div
+        onMouseDown={info.moveElement(id, info)}
+        className='translate-[-50%,0%] absolute -left-[3px] top-[calc(50%-5px)] z-[99999] hidden size-[10px] cursor-nesw-resize bg-blue-600 group-hover:block'
+      ></div>
+      <div
+        onMouseDown={info.moveElement(id, info)}
+        className='translate-[-50%,0%] absolute -top-[3px] left-[calc(50%-5px)] z-[99999] hidden size-[10px] cursor-nesw-resize bg-blue-600 group-hover:block'
+      ></div>
+      <div
+        onMouseDown={info.moveElement(id, info)}
+        className='translate-[-50%,0%] absolute -right-[3px] top-[calc(50%-5px)] z-[99999] hidden size-[10px] cursor-nesw-resize bg-blue-600 group-hover:block'
+      ></div>
+      <div
+        onMouseDown={info.moveElement(id, info)}
+        className='translate-[-50%,0%] absolute -bottom-[3px] left-[calc(50%-5px)] z-[99999] hidden size-[10px] cursor-nesw-resize bg-blue-600 group-hover:block'
       ></div>
     </>
   );
