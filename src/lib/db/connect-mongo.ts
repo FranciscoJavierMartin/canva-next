b071d7566c1f1ecb7668bb64a7b388f7060a7591
Francisco <javiermartincuenca@gmail.com>
2024-05-29 17:55:54 +0200

    Add user model

---
+++ src/lib/db/connect-mongo.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const cached: {
+  connection?: typeof mongoose;
+  promise?: Promise<typeof mongoose>;
+} = {};
+
+export default async function connectMongo() {
+  if (process.env.MONGO_URI) {
+    throw new Error(
+      'Please define the MONGO_URI environment variable inside .env',
+    );
+  }
+
+  if (!cached.connection) {
+    if (!cached.promise) {
+      const opts = {
+        bufferCommands: false,
+      };
+      cached.promise = mongoose.connect(process.env.MONGO_URI!, opts);
+    }
+
+    try {
+      cached.connection = await cached.promise;
+    } catch (e) {
+      cached.promise = undefined;
+      throw e;
+    }
+  }
+
+  return cached.connection;
+}
---
+++ src/lib/db/models/userModel.ts
@@ -0,0 +1,26 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true },
+);
+
+export default models.User || model('User', UserSchema);
