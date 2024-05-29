e2ded32d572321fefc61905d8e5bc43adf7ba89e
Francisco <javiermartincuenca@gmail.com>
2024-05-29 21:32:29 +0200

    WIP

--- src/actions/create-design.ts
+++ src/actions/create-design.ts
@@ -1,6 +1,20 @@
-'use server'
+'use server';
+
+import { JWT } from '@/lib/constants';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { redirect } from 'next/navigation';
 
 export async function createDesign(formData: FormData) {
+  const token = cookies().get(JWT)?.value;
 
-  console.log('Here');
+  if (token) {
+    const userInfo = jwt.verify(token, process.env.JWT_SECRET!);
+    console.log(userInfo);
+    // if (!userInfo) {
+    //   userInfo.
+    // }
+  }else {
+    console.log('Logout')
+  }
 }
