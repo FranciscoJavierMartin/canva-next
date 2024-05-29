06a8eb469177f708ef13556819061daffab943f3
Francisco <javiermartincuenca@gmail.com>
2024-05-29 18:10:53 +0200

    Create model

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -1,28 +1,54 @@
 'use server';
 
-import { registerUserSchema } from '@/lib/validations/registerUserSchema';
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
+import bcrypt from 'bcrypt';
+import { registerUserSchema } from '@/lib/validations/registerUserSchema';
+import userModel from '@/lib/db/models/userModel';
 
 export async function registerUser(
   prev: RegisterFormState<RegisterFormFields>,
   formData: FormData,
 ) {
+  let message: string = '';
   const data = {
     email: formData.get('email'),
     name: formData.get('name'),
     password: formData.get('password'),
   };
 
-  const validatedFields = registerUserSchema.safeParse(data);
+  const validatedFields = registerUserSchema
+    .transform((data) => ({
+      name: data.name.trim(),
+      email: data.email.trim(),
+      password: data.password.trim(),
+    }))
+    .safeParse(data);
 
   if (validatedFields.success) {
+    const { email, name, password } = data;
+    const userExists = await userModel.findOne({ email }).select('+password');
+
+    if (userExists) {
+      message = 'User already exists';
+    } else {
+      const user = await userModel.create({
+        name,
+        email,
+        password: await bcrypt.hash(
+          password!.toString(),
+          process.env.SALT_PASSWORD!,
+        ),
+      });
+
+
+    }
     redirect('/home');
   } else {
   }
 
   return {
-    message: '',
+    message,
     error: validatedFields.error?.flatten().fieldErrors,
   };
 }
