dfe1412f3bc5e74a4936a170b88e5b29fc763269
Francisco <javiermartincuenca@gmail.com>
2024-06-01 12:25:50 +0200

    Get user

--- src/actions/upload-image.ts
+++ src/actions/upload-image.ts
@@ -1,5 +1,21 @@
 'use server';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { redirect } from 'next/navigation';
+import { JWT } from '@/lib/constants';
 
 export async function uploadImage(formData: FormData) {
+  const token = cookies().get(JWT)?.value;
 
+  if (token) {
+    const userInfo: any = jwt.verify(token, process.env.JWT_SECRET!);
+
+    if (userInfo) {
+      console.log(userInfo.id)
+    } else {
+      redirect(`/?show=true&form=login`);
+    }
+  } else {
+    redirect(`/?show=true&form=login`);
+  }
 }
